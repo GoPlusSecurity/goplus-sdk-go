64e090c6814a920453427754ff69a0c4b06b38b3
cong <33095661+cong08@users.noreply.github.com>
2025-06-09 18:58:27 +0800

    feat: support new detection API (#36)

--- api/token/token_security_test.go
+++ api/token/token_security_test.go
@@ -18,3 +18,30 @@ func TestTokenSecurity_Run(t *testing.T) {
 		t.Errorf(data.Payload.Message)
 	}
 }
+
+func TestSolanaTokenSecurity_Run(t *testing.T) {
+	tokenSecurity := NewTokenSecurity(nil)
+	data, err := tokenSecurity.SolanaTokenSecurity([]string{"HZ1JovNiVvGrGNiiYvEozEVgZ58xaU3RKwX8eACQBCt3"})
+
+	if err != nil {
+		t.Errorf(err.Error())
+
+	}
+
+	if data.Payload.Code != errorcode.SUCCESS {
+		t.Errorf(data.Payload.Message)
+	}
+}
+
+func TestSuiTokenSecurity_Run(t *testing.T) {
+	tokenSecurity := NewTokenSecurity(nil)
+	data, err := tokenSecurity.SuiTokenSecurity([]string{"0x40402a987c2f8a71b755561bfbd16c2cbb991e27e609ad148809491c32bacab9::kui::KUI"})
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if data.Payload.Code != errorcode.SUCCESS {
+		t.Errorf(data.Payload.Message)
+	}
+}
